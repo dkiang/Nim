3e24557618f7a765c08d3e566f7c32851fdd0cab
dkiang <dkiang@punahou.edu>
2014-10-20 12:21:36 -1000

    Added KiangNim
    Created constructors, and play method that just returns 1 every time.

---
+++ KiangNim.java
@@ -0,0 +1,20 @@
+public class KiangNim
+{
+  int numberOfPieces = 0; // Stores the original number of pieces
+  
+  public KiangNim()
+  {
+    numberOfPieces = 13;
+  }
+  
+  public KiangNim(int numPieces)
+  {
+    numberOfPieces = numPieces;
+  }
+  
+  public int play(int pieces)
+  {
+    return 1;
+  }
+}
+    
